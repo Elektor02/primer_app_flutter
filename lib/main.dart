fcdeb3032aa1ff193e353f5d5b7783d6c46180b0
Elektor <hectorbiscuit7@gmail.com>
2023-10-27 12:12:33 -0700

    Version BigCard con estilos

--- lib/main.dart
+++ lib/main.dart
@@ -43,15 +43,47 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context){
     var appState = context.watch<MyAppState>();
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Idea aleatoria"),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(onPressed: (){
-            appState.getSiguiente();
-          },
-           child: Text("Siguiente")),
-          ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(idea: appState.current),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: (){
+              appState.getSiguiente();
+            },
+             child: Text("Siguiente")),
+            ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  final WordPair idea;
+
+  const BigCard({
+    super.key,
+    required this.idea 
+  });
+
+  @override
+  Widget build(BuildContext context){
+    final tema = Theme.of(context);
+    final TextStyle = tema.textTheme.displayMedium!.copyWith(
+      color: tema.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: tema.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(idea.asLowerCase, 
+        style: TextStyle,
+        semanticsLabel: "${idea.first}" "${idea.second}",
+        ),
       ),
     );
   }
